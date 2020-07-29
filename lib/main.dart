70d0abafad3ef60ad4603f8b050c716a8accb77c
erinkelsey <erinszilagyi@gmail.com>
2020-07-29 16:03:24 -0500

    Add basic auth, sign up and login with Firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,15 @@ import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 
 void main() {
+  const api = String.fromEnvironment('FIREBASE_API_KEY');
+  print(api);
   runApp(MyApp());
 }
 
@@ -39,14 +43,18 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Orders(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
       ],
       child: MaterialApp(
-          title: 'Trés Chic Shop',
+          title: 'ChicShop',
           theme: ThemeData(
               primarySwatch: Colors.purple,
               accentColor: Colors.pink[100],
               fontFamily: 'Lato'),
-          home: ProductOverviewScreen(),
+          // home: ProductOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlNoAPIKey) async {
+    const api_key = String.fromEnvironment('FIREBASE_API_KEY');
+    final url = urlNoAPIKey + api_key;
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      // throw through to widget
+      throw error;
+    }
+
+    // print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password,
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password,
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=');
+  }
+}
