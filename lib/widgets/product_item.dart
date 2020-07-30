986752f401a6be70b288faa66067dbc09aa3d4ed
erinkelsey <erinszilagyi@gmail.com>
2020-07-29 23:49:33 -0500

    Add an image fade in transition for the product item image.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -31,8 +31,9 @@ class ProductItem extends StatelessWidget {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                 arguments: product.id);
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
         ),
